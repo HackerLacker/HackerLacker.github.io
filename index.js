a667c8d9f536e9cd4a1b1dbccb6955bb112aedf3
HackerLacker <no@gmail.com>
2021-03-13 15:30:41 +0300

    Site without backend!

---
+++ index.js
@@ -0,0 +1,9 @@
+(async () => {
+    const resp = await fetch('/users')
+    const users = await resp.json()
+    let div = ''
+    users.forEach(item => {
+        div += `<p><img class="imgPidors" src="${item.photo}"></p> <p class="namePidors">${item.name.first} ${item.name.last}</p>`
+    })
+    document.getElementById('pidors').innerHTML = div
+})()
